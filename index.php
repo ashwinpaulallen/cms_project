fc95a4ebb621df082c2df8bbf4024b1b9d1a0e04
ashwinpaulallen <ashwin23paul@outlook.com>
2021-01-02 20:17:16 +0530

    added image to posts

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php  include "includes/header.php" ?>
-
-
 <!-- Navigation -->
 <?php  include "includes/navigation.php"?>
 <!-- Page Content -->
@@ -26,9 +24,7 @@
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
-                    $post_content = $row['post_content'];
-                    
-                    
+                    $post_content = $row['post_content'];            
                     ?>
                     
                     <h2>
@@ -38,27 +34,19 @@
                         by <a href="index.php"><?php echo $post_author ?></a>
                     </p>
                     <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
-            <hr>
-            <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-            <hr>
-            <p><?php echo $post_content ?></p>
-            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-            <hr>
-            
-                 
-          <?php      }     
+                    <hr>
+                    <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+                    <hr>
+                    <p><?php echo $post_content ?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <hr>     
+                    <?php
+                }     
             ?>
-            
-
         </div>
-
         <!-- Blog Sidebar Widgets Column -->
         <?php  include "includes/sidebar.php"?>
-
-
     </div>
     <!-- /.row -->
-
     <hr>
     <?php  include "includes/footer.php"?>
